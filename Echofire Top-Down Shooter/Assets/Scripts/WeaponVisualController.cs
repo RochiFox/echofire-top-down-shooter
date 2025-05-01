1706a80657c43a9e11ccf85d4a033ee062b84650
Rochi <rochithefox@gmail.com>
2025-05-01 14:00:14 +0300

    Added: hands position on different weapons

--- Echofire Top-Down Shooter/Assets/Scripts/WeaponVisualController.cs
+++ Echofire Top-Down Shooter/Assets/Scripts/WeaponVisualController.cs
@@ -12,6 +12,16 @@ public class WeaponVisualController : MonoBehaviour
     [SerializeField] private Transform shotgun;
     [SerializeField] private Transform rifle;
 
+    private Transform currentGun;
+
+    [Header("Left hand IK")]
+    [SerializeField] private Transform leftHand;
+
+    void Start()
+    {
+        SwitchOn(pistol);
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -34,6 +44,9 @@ public class WeaponVisualController : MonoBehaviour
     {
         SwitchOffGuns();
         gunTransform.gameObject.SetActive(true);
+        currentGun = gunTransform;
+
+        AttachLeftHand();
     }
 
     private void SwitchOffGuns()
@@ -42,5 +55,13 @@ public class WeaponVisualController : MonoBehaviour
         {
             gunTransforms[i].gameObject.SetActive(false);
         }
+    }
+
+    private void AttachLeftHand()
+    {
+        Transform targetTransform = currentGun.GetComponentInChildren<LeftHandTargetTransform>().transform;
+
+        leftHand.localPosition = targetTransform.localPosition;
+        leftHand.localRotation = targetTransform.localRotation;
     }
 }
