2885a98fbef77399d585f38144cfee8c32e05389
Rochi <rochithefox@gmail.com>
2025-05-08 18:24:21 +0300

    Changed: bullet prefab, target base values

--- Echofire Top-Down Shooter/Assets/Scripts/Target.cs
+++ Echofire Top-Down Shooter/Assets/Scripts/Target.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameObject.layer = LayerMask.NameToLayer("Enemy");
     }
 }
