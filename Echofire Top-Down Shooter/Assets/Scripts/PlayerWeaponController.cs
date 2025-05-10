c2ed299d219642b07ea50e30580ccebe80131222
Rochi <rochithefox@gmail.com>
2025-05-10 17:28:37 +0300

    Added: weapon slots

--- Echofire Top-Down Shooter/Assets/Scripts/PlayerWeaponController.cs
+++ Echofire Top-Down Shooter/Assets/Scripts/PlayerWeaponController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerWeaponController : MonoBehaviour
@@ -7,11 +8,15 @@ public class PlayerWeaponController : MonoBehaviour
 
     [SerializeField] private Weapon currentWeapon;
 
+    [Header("Bullet details")]
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletSpeed;
     [SerializeField] private Transform gunPoint;
 
     [SerializeField] private Transform weaponHolder;
+
+    [Header("Inventory")]
+    [SerializeField] private List<Weapon> weaponSlots;
 
     private void Awake()
     {
@@ -20,9 +25,36 @@ public class PlayerWeaponController : MonoBehaviour
 
     private void Start()
     {
-        player.controls.Character.Fire.performed += context => Shoot();
+        AssignInputEvents();
 
         currentWeapon.ammo = currentWeapon.maxAmmo;
+    }
+
+    private void AssignInputEvents()
+    {
+        PlayerControls controls = player.controls;
+
+        controls.Character.Fire.performed += context => Shoot();
+
+        controls.Character.EquipSlot1.performed += context => EquipWeapon(0);
+        controls.Character.EquipSlot2.performed += context => EquipWeapon(1);
+
+        controls.Character.DropCurrentWeapon.performed += context => DropWeapon();
+    }
+
+    private void EquipWeapon(int index)
+    {
+        currentWeapon = weaponSlots[index];
+    }
+
+    private void DropWeapon()
+    {
+        if (weaponSlots.Count <= 1)
+            return;
+
+        weaponSlots.Remove(currentWeapon);
+
+        currentWeapon = weaponSlots[0];
     }
 
     private void Shoot()
