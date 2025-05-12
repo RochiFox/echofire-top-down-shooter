de1592c552cef640a3bdd1c8ab036a9e2335cf0d
Rochi <rochithefox@gmail.com>
2025-05-12 14:28:35 +0300

    Added: object pool for bullets

--- Echofire Top-Down Shooter/Assets/Scripts/Bullet.cs
+++ Echofire Top-Down Shooter/Assets/Scripts/Bullet.cs
@@ -6,12 +6,12 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private GameObject bulletImpactFX;
 
-    private Rigidbody rb => GetComponent<Rigidbody>(); 
+    private Rigidbody rb => GetComponent<Rigidbody>();
 
     private void OnCollisionEnter(Collision collision)
     {
         CreateImpactFx(collision);
-        Destroy(gameObject);
+        ObjectPool.instance.ReturnBullet(gameObject);
     }
 
     private void CreateImpactFx(Collision collision)
